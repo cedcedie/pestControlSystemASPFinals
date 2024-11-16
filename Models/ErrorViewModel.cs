0da46504146a2f9f24ad1886d9f63cb591ba918a
cedcedie <cedricjamesbulan@gmail.com>
2024-11-16 16:48:26 +0800

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace pestControlSystemASPFinals.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
